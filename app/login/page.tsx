7401bb14a810a8b1fc49dfcf58662e35bc986d8b
Vardanshaswat <60690743+Vardanshaswat@users.noreply.github.com>
2025-07-16 14:53:42 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,9 @@
 import LoginForm from "@/components/LoginForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-// import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // Using alias
-
-// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function LoginPage() {
-  // const session = await getServerSession(authOptions);
 
-  // If already logged in, redirect to homepage
-  // if (session)
   redirect("/homepage");
 
   return (
