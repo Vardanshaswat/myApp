d316a160ec9fed017fffcd1e0bc5db1781dac26f
Vardanshaswat <60690743+Vardanshaswat@users.noreply.github.com>
2025-07-16 15:02:12 +0530

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,18 +2,18 @@
 
 import RegisterForm from "@/components/RegisterForm";
 import { useSession } from "next-auth/react";
-import { JSX, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-export default function RegisterPage(): JSX.Element {
-  const { data: session, status } = useSession();
+export default function RegisterPage() {
+  const sessionResult = useSession();
   const router = useRouter();
 
   useEffect(() => {
-    if (status === "authenticated") {
+    if (sessionResult.status === "authenticated") {
       router.push("/dashboard");
     }
-  }, [status, router]);
+  }, [sessionResult.status, router]);
 
   return (
     <main className="p-6 max-w-md mx-auto">
